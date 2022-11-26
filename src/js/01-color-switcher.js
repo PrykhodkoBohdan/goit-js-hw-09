3bde912cff9d4de9a88d9d8297aacc4beb02a996
pickfix <107954584+pickfix@users.noreply.github.com>
2022-11-26 13:41:28 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,10 +1,28 @@
+const refs = {
+   startBtn: document.querySelector("[data-start]"),
+   stopBtn: document.querySelector("[data-stop]"),
+   interval: null
+};
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+  };
 
-const buttonChange = () => {
+  const onSwithBtn = () =>{
+refs.interval = setInterval(() => {
+  document.body.style.backgroundColor = getRandomHexColor();
+},1000);
+console.log(refs.interval)
+refs.startBtn.setAttribute("disabled", true);
+refs.stopBtn.removeAttribute("disabled");
+  };
 
-return ``
-
-
-}
+  
+const offSwithBtn = () => {
+ clearInterval(refs.interval);
+  refs.stopBtn.setAttribute("disabled",true);
+  refs.startBtn.removeAttribute("disabled");
+};
+  
+refs.startBtn.addEventListener('click',onSwithBtn);
+refs.stopBtn.addEventListener('click',offSwithBtn);
